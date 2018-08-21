745094e02690474dce53db139f33eecb95dba54e
Stefan Pabst <ich@stefanpabst.me>
2018-08-21 18:15:44 +0200

    Fork this commit for next projects

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 </div>
 <footer id="footer" role="contentinfo">
 <div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'annabaus' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'annabaus' ), '<a href="http://stefanpabst.me/">stefanpabst.me</a>' ); ?>
+<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'annabaus' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'Stefan Pabst' ), '<a href="http://stefanpabst.me/">stefanpabst.me</a>' ); ?>
 </div>
 </footer>
 </div>
