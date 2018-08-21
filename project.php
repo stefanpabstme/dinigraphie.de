5a566cbb23170fc9daf177255fa59e47ead4d590
Stefan Pabst <ich@stefanpabst.me>
2018-08-21 19:04:23 +0200

    Duplicated page to project page template

---
+++ project.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Project */ ?>
+<?php get_header(); ?>
+<section id="content" role="main">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<header class="header">
+<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+</header>
+<section class="entry-content">
+  <h1>Projektseite</h1>
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+<?php the_content(); ?>
+<div class="entry-links"><?php wp_link_pages(); ?></div>
+</section>
+</article>
+<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+<?php endwhile; endif; ?>
+</section>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
