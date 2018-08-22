7d426237bf52434e2c2e8f3e3f584539503ae231
Fonzane <timschaffert@yahoo.de>
2018-08-22 20:41:43 +0200

    navbar test

---
+++ functions.js
@@ -0,0 +1,11 @@
+function changeVis () {
+  var navbar = document.getElementById("menu");
+
+  if(navbar.classList.contains('hide')) {
+    navbar.classList.remove('hide');
+    navbar.classList.add('show');
+  } else {
+    navbar.classList.remove('show');
+    navbar.classList.add('hide');
+  }
+}
