2a2e600908fa0624a1ce49b99a32e9162d37541a
Stefan Pabst <ich@stefanpabst.me>
2018-08-27 18:26:37 +0200

    Added carousel

--- project.php
+++ project.php
@@ -13,7 +13,6 @@
   // the query
   $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); //category 3 = projects
   ?>
-
   <?php if ( $wpb_all_query->have_posts() ) : ?>
   <!-- the loop -->
   <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
@@ -25,14 +24,52 @@
   </section>
   <?php endwhile; ?>
   <!-- end of the loop -->
-
-
-      <?php wp_reset_postdata(); ?>
-
+  <?php wp_reset_postdata(); ?>
   <?php else : ?>
       <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
   <?php endif; ?>
 </section>
+
+<h2>Carousel Example</h2>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="la.jpg" alt="Los Angeles" style="width:100%;">
+    </div>
+
+    <div class="item">
+      <img src="chicago.jpg" alt="Chicago" style="width:100%;">
+      Lalala <br />
+      Lalala <br />
+      Lalala <br />
+      Lalala <br />
+      Lalala <br />
+    </div>
+
+    <div class="item">
+      <img src="ny.jpg" alt="New york" style="width:100%;">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
 </article>
 <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
 <?php endwhile; endif; ?>
