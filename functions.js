2faf480392d1416af2ef48dfd6087cd62b1ff36f
Fonzane <timschaffert@yahoo.de>
2018-08-29 15:33:19 +0200

    navButton rotation added

--- functions.js
+++ functions.js
@@ -1,11 +1,14 @@
 function toggleNav () {
   var navbar = document.getElementById("nav_overlay");
+  var navButton = document.getElementById('toggleNav');
 
   if(navbar.classList.contains('hide')) {
     navbar.classList.remove('hide');
     navbar.classList.add('show');
+    navButton.classList.add('rotated')
   } else {
     navbar.classList.remove('show');
     navbar.classList.add('hide');
+    navButton.classList.remove('rotated')
   }
 }
