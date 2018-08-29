02a22c17bde3960fd0c951699720f908a99fca77
Fonzane <timschaffert@yahoo.de>
2018-08-29 15:16:27 +0200

    links im footer hinzugefügt

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
 <div class="container">
   <div id="copyright">
     <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'annabaus' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
+    <a href="<?php echo site_url(); ?>/impressum">Impressum</a>
+    <a href="<?php echo site_url(); ?>/datenschutz">Datenschutz</a>
   </div>
 </div>
 </footer>
