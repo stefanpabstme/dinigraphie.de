7a83333741fdb4a0f9b6751c96c424e610edd8c1
Stefan Pabst <ich@stefanpabst.me>
2018-08-30 18:04:59 +0200

    Removed unused code to configure the single post page to be the new project page

--- entry-footer.php
+++ entry-footer.php
@@ -3,8 +3,7 @@
     <?php _e( 'Categories: ', 'annabaus' ); ?>
     <?php the_category( ', ' ); ?>
   </span>
-  <span class="tag-links"><?php the_tags(); ?></span>
-  <?php if ( comments_open() ) {
-  echo '<span class="meta-sep">|</span> <span class="comments-link"><a href="' . get_comments_link() . '">' . sprintf( __( 'Comments', 'annabaus' ) ) . '</a></span>';
-  } ?>
+  <span class="tag-links">
+    <?php the_tags(); ?>
+  </span>
 </footer>
--- entry.php
+++ entry.php
@@ -19,12 +19,7 @@
   }
   ?>
   <?php edit_post_link(); ?>
-  <?php
-  if (!is_search()) get_template_part('entry', 'meta'); ?>
   </header>
   <?php
-  get_template_part('entry', (is_archive() || is_search() ? 'summary' : 'content'));
-  ?>
-  <?php
-  if (!is_search()) get_template_part('entry-footer');
+  get_template_part('entry', 'content');
   ?>
