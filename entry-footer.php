a4188281c8dbccc878d2437b6a9f9e7ba22db2f9
Stefan Pabst <ich@stefanpabst.me>
2018-09-14 18:34:01 +0200

    Delete all files - Part 1 of git reset --hard to commit c420899bd560e6fcecb0b64d7457fc26b7185e01

--- entry-footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="entry-footer">
-  <span class="cat-links">
-    <?php _e( 'Categories: ', 'annabaus' ); ?>
-    <?php the_category( ', ' ); ?>
-  </span>
-  <span class="tag-links">
-    <?php the_tags(); ?>
-  </span>
-</footer>
