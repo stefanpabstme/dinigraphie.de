90ddfddac5c72f335456d3d51e8b55ec0ab2c3e1
Stefan Pabst <ich@stefanpabst.me>
2018-10-30 00:31:17 +0100

    Responsive navigation

--- functions.js
+++ functions.js
@@ -1,3 +1,18 @@
+//set height of entry image in proportion to screen height
+var w_height = jQuery(window).height();
+var w_width = jQuery(window).width();
+var bg_height = w_height * 0.7;
+var bg_height_mobile = w_height * 0.80;
+
+if (w_height <= w_width) {
+  jQuery( '#entry-image' ).css('height', bg_height);
+}
+
+if (w_height > w_width) {
+  jQuery( '#entry-image' ).css('height', bg_height_mobile);
+}
+
+
 //Navigation
 function toggleNav () {
   var navbar = document.getElementById("menu-main");
@@ -14,25 +29,14 @@ function toggleNav () {
   }
 }
 
-//set height of entry image in proportion to screen height
-var w_height = jQuery(window).height();
-var w_width = jQuery(window).width();
-var bg_height = w_height * 0.7;
-var bg_height_mobile = w_height * 0.80;
-
-if (w_height <= w_width) {
-  jQuery( '#entry-image' ).css('height', bg_height);
-}
-
-if (w_height > w_width) {
-  jQuery( '#entry-image' ).css('height', bg_height_mobile);
-}
-
 
 $(document).ready(function() {
   //Document ready halt :D
-  toggleNav();
+  if (w_height > w_width) {
+    toggleNav();
+  }
 });
+
 
 $(window).on("load", function() {
     //Startet erst nachdem alle Bilder etc geladen sind
