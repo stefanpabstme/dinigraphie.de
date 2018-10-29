cbaa748cae327aa8b781a22294a4f9b99af2650a
Stefan Pabst <ich@stefanpabst.me>
2018-10-29 19:12:10 +0100

    Some small fixes

--- page.php
+++ page.php
@@ -6,7 +6,6 @@
 <h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
 </header>
 <section class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 <?php the_content(); ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
