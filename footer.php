d49f6568a83572fa92c9717b5159cfc340db2fb3
Stefan Pabst <ich@stefanpabst.me>
2019-01-22 20:18:50 +0100

    footer icons

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <footer id="footer" role="contentinfo">
       <div class="container">
         <div id="socialmedia">
-          <a href="https://www.facebook.com/Dinigraphie/" target="_blank">FB ICON</a>&nbsp;<a href="https://www.instagram.com/dinigraphie/" target="_blank">INSTA ICON</a>
+          <a href="https://www.facebook.com/Dinigraphie/" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/facebookicon_footer_220119.png" class="ftr_icon" alt="fb_logo" /></a>&nbsp;<a href="https://www.instagram.com/dinigraphie/" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/instagramicon_footer_220119.png" class="ftr_icon" alt="insta_logo" /></a>
         </div>
           <div id="copyright">
             <?php echo sprintf( __( '%1$s %2$s'), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?> /
